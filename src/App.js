d09590adfae3871224c4075a6006825fd0045a67
Samantha Arellano <Sambollano54@gmail.com>
2021-07-30 18:54:52 -0500

    .

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import About from './components/About';
+import Contact from './components/Contact';
+import Project from './components/Project';
+
+function App() { 
+  return (
+    <div className="App">
+        <Router>
+          <Nav />
+          <Route component={About} exact path="/about"/> 
+          <Route component={Contact} exact path="/contact"/>
+          <Route component={Project} exact path="/project"/>
+        </Router>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
